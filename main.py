2f36bfcd626dc3c36e3e1b31f616c1f27d3eb542
MrDigiTarun14 <tarun.chandraprakash@gmail.com>
2025-11-25 11:11:35 -0500

    Completed the project

--- main.py
+++ main.py
@@ -1,23 +1,63 @@
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-import pygame
-from logger import log_state
+import pygame, sys
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_SHOOT_COOLDOWN_SECONDS
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock_object = pygame.time.Clock()
+dt = 0
 
-while True:
-    log_state()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-    
-    screen.fill("black")
-    pygame.display.flip()
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
+
+asteroids = pygame.sprite.Group()
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = (updatable)
+asteroid_field = AsteroidField()
+
+shots = pygame.sprite.Group()
+Shot.containers = (shots, updatable)
+
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player((0.5 * SCREEN_WIDTH), (0.5 * SCREEN_HEIGHT), PLAYER_SHOOT_COOLDOWN_SECONDS)
+
+    while True:
+        log_state(updatable, drawable, asteroids, shots)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        
+        for item in drawable:
+            item.draw(screen)
+            
+        for shot in shots:
+            shot.draw(screen)
+            
+        pygame.display.flip()
+        
+        dt = clock_object.tick(60) / 1000
+        updatable.update(dt)
+        
+        for asteroid in asteroids:
+            if player.collides_with(asteroid) == True:
+                log_event("player_hit")                
+                print("Game Over!")
+                sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid) == True:
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
 
 
 if __name__ == "__main__":
